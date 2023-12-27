327c71a37121ef2596d7970ea36171736d9bf554
Sayan Chatterjee <70452470+sayan4545@users.noreply.github.com>
2023-12-27 22:09:16 +0000

    conversion

---
+++ Conversion_01.js
@@ -0,0 +1,27 @@
+let score = 100;
+console.log(typeof score);
+let scoreInNumber = Number(score);
+console.log(typeof scoreInNumber);
+console.log(scoreInNumber);
+
+let id = "123";
+let idInNumber = Number(id);
+console.log(typeof id);
+console.log(typeof idInNumber);
+console.log(idInNumber);
+
+let accountId = null
+accountIdInNumber = Number(accountId);
+console.log(accountIdInNumber);
+
+let activeAccounts = undefined;
+let activeAccountsInNumber = Number(activeAccounts);
+console.log(activeAccountsInNumber);
+
+let isLoggedIn = 1;
+let isLoggedInInBoolean = Boolean(isLoggedIn);
+console.log(isLoggedInInBoolean);
+
+let name = "";//empty string 
+let nameInBoolean = Boolean(name);
+console.log(nameInBoolean);
