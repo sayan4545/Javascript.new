b778ae68b7d73b49c9588ffb7f97d5a817a1d30b
Sayan Chatterjee <70452470+sayan4545@users.noreply.github.com>
2024-01-01 20:38:07 +0000

    Objects continued

--- Objects_2.js
+++ Objects_2.js
@@ -1,5 +1,5 @@
 const tinderUser = new Object();
-//console.log(tinderUser);
+console.log(tinderUser);
 tinderUser.id = "123abc";
 tinderUser.displayName ="Sayan";
 tinderUser.loggedIn = true;
@@ -16,5 +16,42 @@ tinderUser.fullName ={
 // console.log(tinderUser.fullName.firstName);
 // console.log(typeof tinderUser.fullName);
 // console.log(typeof tinderUser.loggedIn);
-console.log(tinderUser.fullName.userName.firstName);
+//console.log(tinderUser.fullName.userName.firstName);
 
+
+//To merge to objects togeteher.
+const obj1 = {1:"a",2:"b"};
+const obj2 = {3:"c",4:"d"};
+// const obj3 = Object.assign({},obj1,obj2);
+// const obj4 = Object.assign(obj1,obj2);
+// console.log(obj3);
+// console.log(obj3===obj4)
+const obj3 = {...obj1,...obj2};//using the spread operator
+console.log(obj3);
+
+
+const obj_db =[
+    {id:1,email:"sayan@gmail.com"},
+    {id:2,email:"h@gmail.com"},
+    {id:3,email:"jeevan@gmail.com"}
+]
+
+console.log(typeof obj_db);
+console.log(obj_db);
+console.log(obj_db[0].id+" "+obj_db[0].email );
+
+console.log(Object.keys(tinderUser));//This gives an array as output
+console.log(Object.values(tinderUser));
+
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('loggedIn'));
+
+
+const course ={
+    name:"JS in hindi",
+    courseInstructor:"sayan Chatterjee"
+}
+
+course.courseInstructor;
+const{courseInstructor:instructor}=course;//Extracting the courseInstructor from the object course.
+console.log(instructor);
