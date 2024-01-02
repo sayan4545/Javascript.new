7dd9441ae90abfd99e2321ff953894a62eebd641
Sayan Chatterjee <70452470+sayan4545@users.noreply.github.com>
2024-01-02 23:03:01 +0000

    Control Flow

--- ControlFlow.js
+++ ControlFlow.js
@@ -1,5 +1,12 @@
-if(2==='2'){
-    console.log("Executed");
+// if(2==='2'){
+//     console.log("Executed");
+// }else{
+//     console.log("Not executed");
+// }
+const userLoggedIn = true;
+const debitCard =true;
+if(userLoggedIn&&debitCard){
+    console.log("Allowed to buy");
 }else{
-    console.log("Not executed");
+    console.log("Not allowed access to buy!");
 }
