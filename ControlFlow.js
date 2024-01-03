031e909d5d559e1ae053a7799f3a8e56670405e1
Sayan Chatterjee <70452470+sayan4545@users.noreply.github.com>
2024-01-03 17:32:11 +0000

    Control Flow

--- ControlFlow.js
+++ ControlFlow.js
@@ -3,10 +3,27 @@
 // }else{
 //     console.log("Not executed");
 // }
+
+
 const userLoggedIn = true;
 const debitCard =true;
+const userLoggedInFromGoogle = true;
+const userLoggedInFromEmail = false;
+
 if(userLoggedIn&&debitCard){
     console.log("Allowed to buy");
-}else{
+
+}
+
+if(userLoggedInFromGoogle||userLoggedInFromEmail){
+    console.log("Allowed to buy");
+}
+
+else{
     console.log("Not allowed access to buy!");
 }
+
+let age = 29;
+if(age>=18)console.log("Can Drive");
+else if(age>=60)console.log("Not eligible for a re issue");
+else console.log("Cant Drive");
