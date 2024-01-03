671eeb12a6f2f2ca7fbc9cf1aa08db9b19a432da
Sayan Chatterjee <70452470+sayan4545@users.noreply.github.com>
2024-01-03 17:42:36 +0000

    Truthy_and_Falsy

---
+++ Truthy_Falsy.js
@@ -0,0 +1,12 @@
+const userEmail ="s@gmail.com";
+if(userEmail){
+    console.log("Got user email: "+ userEmail);
+}else{
+    console.log("Dont have user email");
+}
+
+//Nullish Coalesence Operator: null and undefined
+let val1;
+val1 =10??null;
+console.log(val1);
+console.log(val1);
